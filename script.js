8a3949edd8ab5fe0e322615bd2408c1cccf54b07
MuhammadIbrar21 <114849910+MuhammadIbrar21@users.noreply.github.com>
2024-03-12 08:32:41 +0500

    add left movement even

--- script.js
+++ script.js
@@ -7,9 +7,16 @@ slideContent();
 function slideContent() {
 
     let triggerPoint = window.innerHeight / 5 * 4;
+    console.log(triggerPoint)
 
-    boxes.forEach(box => {
+    boxes.forEach((box, index) => {
+
         let top = box.getBoundingClientRect().top;
+
+        if (index == 1) {
+            console.log(top)
+        }
+
         if (top < triggerPoint) {
             box.classList.add('show')
         } else {
